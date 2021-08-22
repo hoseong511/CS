afee3a0439df08f81d066e264971304dbed8d5d0
hoseong511 <thdghtjd115@naver.com>
2021-08-22 23:01:04 +0900

    Create CS

---
+++ C, C++/C/CreateProject/CreateProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CreateProject
+//
+//  Created by Ho Ho on 2021/08/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ C, C++/C/VariableConstance/VariableConstance/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  VariableConstance
+//
+//  Created by Ho Ho on 2021/08/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int age = 12;
+    printf("age에 할당된 값은 %d 입니다. \n", age);
+    age = 13;
+    printf("age에 할당된 값은 %d 입니다. \n", age);
+    
+    return 0;
+}
