0df723e050962b205406e039b109562e62ad88d2
hoseong511 <thdghtjd115@naver.com>
2021-08-26 08:26:46 +0900

    Update 3.scanf: 주소값과 형식지정자

--- C, C++/C/3.scanf/main.c
+++ C, C++/C/3.scanf/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  int a;
-  scanf("%d", &a);
-  printf("입력한 숫자는 %d입니다.\n", a); 
+  // int a;
+  // scanf("%d", &a);
+  // printf("입력한 숫자는 %d입니다.\n", a); 
+  // double b;
+  // scanf("%lf", &b);
+  // printf("%.2f\n", b);
+  int c, d;
+  scanf("%d %d", &c, &d);
+  printf("%d %d", d, c);
+  // scanf("%1d%1d%1d",) 붙어있는 입력값을 1자리씩 데이터를 할당할 수 있다.
   return 0;
+
 }
