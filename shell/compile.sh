af4994b94a5ab3f019bb0679e57764a32701c36d
hoseong511 <thdghtjd115@naver.com>
2021-09-13 13:41:24 +0900

    Modification of compile.sh: delete -e 's/ //g'

--- shell/compile.sh
+++ shell/compile.sh
@@ -6,7 +6,7 @@
 #    By: hossong <hossong@student.42seoul.kr>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/09/13 11:38:54 by hossong           #+#    #+#              #
-#    Updated: 2021/09/13 13:34:38 by hoho             ###   ########.fr        #
+#    Updated: 2021/09/13 13:40:39 by hoho             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@
 # Command : ftcc
 # `ftcc` command is a build and execute your project.
 
-if [ -z `grep -n 'ftcc' ~/.bash_profile | sed -e 's/:.*//g' -e 's/ //g'` ] ; then
+if [ -z `grep -n 'ftcc' ~/.bash_profile | sed -e 's/:.*//g'` ] ; then
 	echo "alias ftcc='gcc -Wall -Wextra -Werror *.c -o exe && ./exe && rm exe'" >> ~/.bash_profile
 	val=`grep -n 'bash_profile' ~/.zshrc | sed -e 's/:.*//g'`
 	if [ -z $val ] ; then
