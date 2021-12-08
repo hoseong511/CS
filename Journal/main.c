72760390a8848434f098c5dff4902dbe61681469
hoseong511 <thdghtjd115@naver.com>
2021-12-08 19:47:03 +0900

    docs: :memo: Update Journal : 21-12-08.md #프로세스 #스레드 #NULL #역참조

---
+++ Journal/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (void)
+{
+	char *ptr = NULL;
+	char *ptr2;
+
+	printf("The value of ptr is %p\n", ptr);
+	printf("The value of ptr2 is %p\n", ptr2);
+	return 0;
+}
