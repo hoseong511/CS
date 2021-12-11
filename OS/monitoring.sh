4ea00be4a7b74c45ae1c1395cdbd624dff9cfc34
hoseong511 <thdghtjd115@naver.com>
2021-12-11 23:25:49 +0900

    Update monitoring.sh : 릴리즈날짜 수정 중..

---
+++ OS/monitoring.sh
@@ -0,0 +1,6 @@
+echo "Brodcast message from `whoami`@`hostname` (`who | awk '{print $2}'`) (`date +"%a %b %d %T %Y"`):"
+echo ""
+echo "	#Architecture: `uname -a`"
+echo "	#CPU physical : "
+echo "	#vCPU : "
+
