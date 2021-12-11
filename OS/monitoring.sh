58143c74ceef7a0b5e8dd3531fd166002fe96c6c
hoseong511 <thdghtjd115@naver.com>
2021-12-12 01:34:58 +0900

    #9 issue : crontab 쉘스크립트 실행x

--- OS/monitoring.sh
+++ OS/monitoring.sh
@@ -1,6 +1,7 @@
-echo "Brodcast message from `whoami`@`hostname` (`who | awk '{print $2}'`) (`date +"%a %b %d %T %Y"`):"
-echo ""
-echo "	#Architecture: `uname -a`"
-echo "	#CPU physical : "
-echo "	#vCPU : "
+# echo "Brodcast message from `whoami`@`hostname` (`who | awk '{print $2}'`) (`date +"%a %b %d %T %Y"`):"
+wall "
+	#Architecture: `uname -a`
+	#CPU physical : 
+	#vCPU : 
+"
 
