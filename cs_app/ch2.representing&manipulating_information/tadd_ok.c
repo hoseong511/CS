ee694953af02bb45dafc9f62e254b0ba19761e04
hoseong511 <thdghtjd115@naver.com>
2022-05-12 17:14:16 +0900

    Update cs:app- tadd_ok.c

--- cs_app/ch2.representing&manipulating_information/tadd_ok.c
+++ cs_app/ch2.representing&manipulating_information/tadd_ok.c
@@ -12,6 +12,12 @@ int	tadd_ok(int x, int y)
 		return 1;
 }
 
+int	buggy_tadd_ok(int x, int y)
+{
+	int	sum = x + y;
+	return (sum - x == y) && (sum -y == x);
+}
+
 int	main(void)
 {
 	int x = __INT_MAX__;
@@ -20,4 +26,9 @@ int	main(void)
 		printf("tadd_ok: %d\n", x + y); 
 	else 
 		printf("overflow!: %d\n", x + y);
+	if (buggy_tadd_ok(x, y))
+		printf("tadd_ok: %d\n", x + y); 
+	else 
+		printf("overflow!: %d\n", x + y);
+	
 }
