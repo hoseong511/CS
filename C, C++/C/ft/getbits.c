b453a115284fc7f7939da96f2a16fd91c29b4b43
hoseong511 <thdghtjd115@naver.com>
2022-06-19 15:11:09 +0900

    Update k&r : 예제 2-6, setbits, invert, 비트연산활용

---
+++ C, C++/C/ft/getbits.c
@@ -0,0 +1,46 @@
+
+unsigned	getbits(unsigned x, int p, int n)
+{
+
+	int b = ~(~0 << n);
+	int c1 = (1 << n);
+	int c2 = ~(1 << n);
+	return (x >> (p + 1 - n)) & b;
+}
+
+/* ex: 
+		x = 0b110101
+		p = 4
+		n = 3
+		y = 0b100100
+
+	4~2 bit		=> a = 0b10000 
+	5th bit		=> b = 0b100000
+	1st bit		=> c = 0b01
+*/
+
+unsigned	setbits(unsigned x, int p, int n, unsigned y)
+{
+	int	a = (y & ~(~0 << n)) << (p + 1 - n);
+	int	b = x & (~0 << (p + 1));
+	int	c = x & ~(~0 << (p + 1 - n));
+	return (a + b + c);
+}
+
+unsigned	invert(unsigned x, int p, int n)
+{
+	int	a1 = (~getbits(x, p, n)) & ~(~0 << n);
+	int	a2 = a1 << (p + 1 - n);
+	int	b = x & (~0 << (p + 1));
+	int	c = x & ~(~0 << (p + 1 - n));
+	return (a2 + b + c);
+}
+
+int	main(void)
+{
+	unsigned a = getbits(0b100000111011, 6, 3);
+	(void) a;
+	unsigned b = setbits(0b111101, 4, 3, 0b100001);
+	unsigned c = setbits(0b111101, 4, 3, 0b100101);
+	c = invert(0b110101, 4, 3);
+}
